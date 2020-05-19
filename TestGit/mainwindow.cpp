9d6f5f88aa50a7d40c56b9bdc42ca5837d7204a1
Lunar-YZ <yz757865041@gmail.com>
2020-05-19 15:05:50 +0800

    add 4

--- TestGit/mainwindow.cpp
+++ TestGit/mainwindow.cpp
@@ -5,6 +5,10 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
   // 1
+  // add 11
+  // add 22
+  // add 33
+  // add 44
 }
 
 MainWindow::~MainWindow() { delete ui; }
