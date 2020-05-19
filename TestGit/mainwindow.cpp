929944f7c3988085789c8e319f96c6f73a34020b
Lunar-YZ <yz757865041@gmail.com>
2020-05-19 14:08:23 +0800

    1

--- TestGit/mainwindow.cpp
+++ TestGit/mainwindow.cpp
@@ -2,14 +2,9 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-      , ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+  // 1
 }
 
-MainWindow::~MainWindow()
-{
-  delete ui;
-}
-
+MainWindow::~MainWindow() { delete ui; }
